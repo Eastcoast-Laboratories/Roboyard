76794e70f3b28d2b36d541007bc9d21ce75f2a41
Ruben Barkow-Kuder <rubo77@users.noreply.github.com>
2025-03-14 09:46:23 +0100

    add z-indices while robots move: set walls west and south of robots above robots (higher z-index) and north and east underneath.

--- app/src/main/java/roboyard/eclabs/ZIndexConstants.java
+++ app/src/main/java/roboyard/eclabs/ZIndexConstants.java
@@ -1,14 +1,26 @@
 package roboyard.eclabs;
 
 /**
- * Constants for Z-index values to control drawing order of game objects
+ * Constants for z-index values to control drawing order of game objects
  */
 public class ZIndexConstants {
-    public static final int BACKGROUND = 0;
-    public static final int GRID = 10;
-    public static final int TARGET = 20;
-    public static final int ROBOT_MARKER = 30;
-    public static final int ROBOT = 40;
-    public static final int WALL = 50;
-    public static final int UI_ELEMENT = 100;
+    // Background elements (lowest z-index)
+    public static final int BACKGROUND = 10;
+    
+    // Grid elements
+    public static final int GRID = 20;
+    
+    // Target markers
+    public static final int TARGET = 30;
+    
+    // Robot markers (the transparent markers showing starting positions)
+    public static final int ROBOT_MARKER = 35;
+    
+    // Base z-index for game objects (walls and robots)
+    // Actual z-index will be calculated based on position
+    // each element gets one z-index value lower than the previous one
+    public static final int GAME_OBJECT_BASE = 9000;
+    
+    // UI elements (highest z-index)
+    public static final int UI_ELEMENT = 10000;
 }
